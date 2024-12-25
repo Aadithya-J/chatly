6de417509bd2b6b3fb5f2d1674e1ac6a6a6ff37c
Aadithya-J <jlaadithya@gmail.com>
2024-12-25 14:55:35 +0530

    implement dynamic routing for server channel navigation and redirect to first channel

--- src/app/(main)/[serverId]/page.tsx
+++ src/app/(main)/[serverId]/page.tsx
@@ -1,5 +1,28 @@
-export default function Page(){
-    return (
-        <h2>Hello World!</h2>
-    )
+'use client';
+
+import { useFirstChannelId } from "~/hooks/api-utils"
+import { useParams, useRouter } from 'next/navigation'
+import { useEffect } from "react";
+export default function Page() {
+  console.log("Page")
+  const params = useParams()
+  const serverId = Array.isArray(params.serverId) ? params.serverId[0] ?? '' : params.serverId ?? ''
+  const { firstChannelId, isLoading, isError } = useFirstChannelId(serverId)
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!isLoading && !isError && firstChannelId) {
+      router.push(`/${serverId}/${firstChannelId}`)
+    }
+  }, [firstChannelId, serverId, isLoading, isError, router])
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>Error loading channels</h2>;
+  }
+
+  return <h2>Hello World!</h2>
 }
